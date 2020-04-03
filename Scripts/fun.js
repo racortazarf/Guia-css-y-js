bfa596c6997528e01ba8ac6ef4c715213f44ed41
racortazarf <racortazarf@libertadores.edu.co>
2020-04-03 10:34:51 -0500

    Se agrega script

---
+++ Scripts/fun.js
@@ -0,0 +1,8 @@
+function cambiocolor(){
+      obj.addEventListener('mouseover' , function())
+      this.classList.add('resaltar');
+  })
+      obj.addEventListener('mouseout', function() {
+      this.classList.remove('resaltar');
+      })
+  }
