8bc4a9c96dc7c4457237883dd62fbea4ce2efcef
racortazarf <racortazarf@libertadores.edu.co>
2020-04-03 21:27:40 -0500

    primer prueba responsive

--- Scripts/fun.js
+++ Scripts/fun.js
@@ -1,8 +1,24 @@
-function cambiocolor(){
-      obj.addEventListener('mouseover' , function())
-      this.classList.add('resaltar');
-  })
-      obj.addEventListener('mouseout', function() {
-      this.classList.remove('resaltar');
-      })
+/*@media screen and (max-width: 700px){
+ .encabezado{
+  font-size:18px;
+}
+}
+@media screen and (max-width: 400px){
+.contenedor{
+font-size:10px;
+}
+.encabezado{
+font-size:16px;
+}
+}*/
+@media screen and (min-width: 601px) {
+    textoencabezado {
+      font-size: 30px;
+    }
+  }
+  
+  @media screen and (max-width: 600px) {
+    textoencabezado {
+      font-size: 15px;
+    }
   }
